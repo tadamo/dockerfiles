c1ee174da9df1c04269710a6b07c77fa0e5b95fc
Tom Adamo <tadamo@buffalo.edu>
2023-06-09 13:28:02 -0400

    updated

--- k8s-util/app/bin/k8s-pod-metrics.pl
+++ k8s-util/app/bin/k8s-pod-metrics.pl
@@ -6,9 +6,18 @@ use Mojo::JSON qw(decode_json);
 use Mojo::URL;
 use Mojo::Util qw(dumper);
 use Mojo::Log;
+use Text::Table;
+use Time::Piece;
 
 my $log = Mojo::Log->new;
 my $ua  = Mojo::UserAgent->new;
+my $tb  = Text::Table->new(
+    "NAMESPACE", "NAME",     "PHASE",     "START-TIME",
+    "NODE",      "IP",       "|",         "NAME",
+    "READY",     "RESTARTS", "CPU-REQ",   "CPU-LIMIT",
+    "CPU-USAGE", "MEM-REQ",  "MEM-LIMIT", "MEM-USAGE",
+    "MEM%"
+);
 
 ###############################################################################
 ###############################################################################
@@ -55,7 +64,7 @@ $log->debug( "k8s_api_server_request_headers, $_: "
 ###############################################################################
 
 # Search kubapi non-blocking for multiple terms concurrently
-my ( $pod_info_ref, $pod_metrics_ref );
+my ( $pods_info_ref, $pods_metrics_ref );
 async sub get_pods_details_p ($ua) {
     my @results = await Mojo::Promise->all(
         $ua->get_p(
@@ -66,8 +75,8 @@ async sub get_pods_details_p ($ua) {
                 $k8s_api_server_request_headers->to_hash
         ),
     );
-    $pod_info_ref    = $results[0]->[0]->result->json;
-    $pod_metrics_ref = $results[1]->[0]->result->json;
+    $pods_info_ref    = $results[0]->[0]->result->json;
+    $pods_metrics_ref = $results[1]->[0]->result->json;
     return;
 }
 
@@ -76,3 +85,58 @@ get_pods_details_p($ua)->wait;
 ###############################################################################
 ###############################################################################
 ###############################################################################
+
+sub utc_to_local ($utc_ts) {
+    my $utc_tp   = Time::Piece->strptime( $utc_ts, '%Y-%m-%dT%H:%M:%SZ' );
+    my $local_tp = localtime( $utc_tp->epoch );
+    return $local_tp->strftime('%Y-%m-%d %I:%M:%S %p');
+}
+
+for my $pod_info_ref ( @{ $pods_info_ref->{items} } ) {
+    my @tb_pod_row = (
+        $pod_info_ref->{metadata}{namespace},
+        $pod_info_ref->{metadata}{name},
+        $pod_info_ref->{status}{phase},
+        utc_to_local( $pod_info_ref->{status}{startTime} ),
+        $pod_info_ref->{spec}{nodeName},
+        $pod_info_ref->{status}{podIP},
+    );
+
+    my $pod_metrics_ref = (
+        grep {
+                    $_->{namespace} eq $pod_info_ref->{metadata}{namespace}
+                and $_->{name} eq $pod_info_ref->{metadata}{name}
+        } @{ $pods_metrics_ref->{items} }
+    )[0];
+
+    use Data::Dumper qw(Dumper);
+    warn Dumper $pod_metrics_ref;
+
+    for my $container_ref ( @{ $pod_info_ref->{spec}{containers} } ) {
+        my $container_status_ref
+            = ( grep { $_->{name} eq $container_ref->{name} }
+                @{ $pod_info_ref->{status}{containerStatuses} } )[0];
+        my @tb_container_row = (
+            "",
+            $container_ref->{name},
+            $container_status_ref->{ready},
+            $container_status_ref->{restartCount},
+            $container_ref->{resources}{requests}{cpu},
+            $container_ref->{resources}{requests}{memory},
+            "",
+            $container_ref->{resources}{limits}{cpu},
+            $container_ref->{resources}{limits}{memory},
+            "",
+        );
+        push @tb_pod_row, @tb_container_row;
+    }
+
+    $tb->load( \@tb_pod_row );
+
+    last;
+}
+
+###############################################################################
+###############################################################################
+###############################################################################
+print $tb;
