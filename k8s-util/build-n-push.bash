9cbb72f8cdba4145b89071b46f676db936613b40
Tom Adamo <tadamo@buffalo.edu>
2023-08-28 08:31:19 -0400

    updated

--- k8s-util/build-n-push.bash
+++ k8s-util/build-n-push.bash
@@ -4,4 +4,4 @@ set -o nounset  # abort on unbound variable
 set -o pipefail # don't hide errors within pipes
 set -o xtrace   # Print command traces before executing command
 
-docker buildx build --no-cache --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t tadamo/k8s-util:latest .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t tadamo/k8s-util:latest .
